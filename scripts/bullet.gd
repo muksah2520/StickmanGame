3a1d83c8dfaebb4cb02d2a098dfb1c9ae120ab75
muksah2520 <mukulsahushoma@gmail.com>
2025-12-14 13:24:01 +0530

    bullet kills player

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,6 +4,9 @@ var speed = 2000
 func _physics_process(delta: float) -> void:
 	position+=transform.x*delta*speed
 func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.queue_free()
 	queue_free()
+	
 	
 	
